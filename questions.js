140a6599ba7526446d3ba62f0c7883aa8b85797b
LegalQuizzes <newages33@yahoo.com>
2025-04-13 01:18:07 +0300

    questions.js

--- questions.js
+++ questions.js
@@ -1,21 +1,47 @@
-
 const questions = [
   {
     id: "q1",
-    text: "Care dintre acestea sunt limbaje de programare?",
+    text: "Sub sancțiunea nulității relative nu pot cumpăra drepturi litigioase:",
     answers: [
-      { text: "JavaScript", correct: true },
-      { text: "Python", correct: true },
-      { text: "HTML", correct: false }
+      { text: "A. judecătorii, atunci când drepturile litigioase sunt de competența instanței superioare celei la care își desfășoară activitatea;", correct: false },
+      { text: "B. părinții pentru bunurile persoanei pe care o reprezintă;", correct: true },
+      { text: "C. mandatarii pentru bunurile pe care sunt însărcinați să le vândă, chiar dacă au fost împuterniciți în mod expres în acest sens;", correct: false }
     ]
   },
   {
     id: "q2",
-    text: "Ce sunt planetele din Sistemul Solar?",
+    text: "Sublocațiunea:",
     answers: [
-      { text: "Marte", correct: true },
-      { text: "Luna", correct: false },
-      { text: "Jupiter", correct: true }
+      { text: "A. spre deosebire de cesiune, nu poate fi interzisă;", correct: false },
+      { text: "B. este permisă doar dacă este încheiată sub formă autentică;", correct: false },
+      { text: "C. când are ca obiect o locuință este condiționată de acordul scris al locatorului;", correct: true }
+    ]
+  },
+  {
+    id: "q3",
+    text: "Proprietatea periodică:",
+    answers: [
+      { text: "A. este un caz de coproprietate forțată;", correct: true },
+      { text: "B. poate înceta prin partaj judiciar;", correct: false },
+      { text: "C. poate avea ca obiect doar bunurile imobile", correct: false }
+    ]
+  },
+  {
+    id: "q4",
+    text: "Actul de opțiune succesorală:",
+    answers: [
+      { text: "A. nu poate fi exercitat valabil și înainte de deschiderea succesiunii;", correct: true },
+      { text: "B. este în toate cazurile indivizibil și irevocabil;", correct: false },
+      { text: "C. poate consta și în acțiunea unui succesibil, prin care solicită instanței judecătorești nedemnitatea unui alt succesibil;", correct: true }
+    ]
+  },
+  {
+    id: "q5",
+    text: "Clauza de inalienabilitate:",
+    answers: [
+      { text: "A. poate fi instituită pentru o durată de cel mult 49 de ani, doar prin convenția părților;", correct: false },
+      { text: "B. este subînțeleasă în antecontracte având ca obiect transmiterea în viitor, prin încheierea de contracte, a dreptului de proprietate asupra unui bun mobil sau imobil, după caz, dacă prin lege nu se prevede altfel;", correct: true },
+      { text: "C. în lipsa înscrierii în cartea funciară este lovită de nulitate relativă.", correct: false }
     ]
   }
 ];

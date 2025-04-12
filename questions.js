499a5d688164c0742ac33f12fe2decddcd89f13c
LegalQuizzes <newages33@yahoo.com>
2025-04-13 00:50:29 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,21 @@
+
+const questions = [
+  {
+    id: "q1",
+    text: "Care dintre acestea sunt limbaje de programare?",
+    answers: [
+      { text: "JavaScript", correct: true },
+      { text: "Python", correct: true },
+      { text: "HTML", correct: false }
+    ]
+  },
+  {
+    id: "q2",
+    text: "Ce sunt planetele din Sistemul Solar?",
+    answers: [
+      { text: "Marte", correct: true },
+      { text: "Luna", correct: false },
+      { text: "Jupiter", correct: true }
+    ]
+  }
+];
